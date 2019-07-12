13126c3c29647497f3bfc907d0f714f7b1ff0b72
Lawrence LCI <lcheung@lasallecollgevancouver.com>
2019-07-11 21:07:33 -0700

    [VGP232] Created first project

---
+++ VGP232/Week1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello VGP232 Class!");
+            Console.ReadKey();
+        }
+    }
+}
