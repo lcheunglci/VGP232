b93f201262c960bc30902ed3a757dde12c7338b1
Lawrence LCI <lcheung@lasallecollgevancouver.com>
2020-02-06 22:54:19 -0800

    [VGP232] Add assignment 3 template

---
+++ VGP232/Assignment3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace Assignment3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButtonPressed(object sender, RoutedEventArgs e)
+        {
+            SpriteGenerator.Generate();
+        }
+
+        private void BrowseButtonPressed(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                selectedFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ShowButtonPressed(object sender, RoutedEventArgs e)
+        {
+            string pathToSpriteSheet = System.IO.Path.Combine(selectedFolder.Text, "spritesheet.png");
+            var image = (new ImageSourceConverter()).ConvertFromString(pathToSpriteSheet) as ImageSource;
+            previewImage.Width= image.Width;
+            previewImage.Height = image.Height;
+            previewImage.Source = image;
+        }
+
+        private void FindButtonPressed(object sender, RoutedEventArgs e)
+        {
+            string pathToSpriteSheet = selectedFolder.Text;
+            System.Diagnostics.Process.Start("explorer", pathToSpriteSheet);
+
+        }
+    }
+}
---
+++ VGP232/Assignment3/SpriteGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Assignment3
+{
+    public class SpriteGenerator
+    {
+        public static void Generate()
+        {
+            // Modify these three/four variables and use WPF Controls to trigger this function 
+
+            // TODO: parameterize it
+            string myFolder = "mystuff";
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), myFolder);
+            string output = "spritesheet.png";
+            int columns = 6;
+
+            if (!path.EndsWith(@"\"))
+            {
+                path = path + @"\";
+            }
+            string outputPath = path + output;
+
+            if (File.Exists(outputPath))
+            {
+                File.Delete(outputPath);
+            }
+
+            // Everything below here should be automatic
+            Console.WriteLine("Analyzing input files:\n{0}", path);
+            string[] files = Directory.GetFiles(path, "*.png");
+            int fileCount = files.Length;
+            if (fileCount > 0)
+            {
+                Console.WriteLine("{0} files found, analyzing dimensions.", files.Length);
+
+                int maxWidth = 0;
+                int maxHeight = 0;
+                foreach (string f in files)
+                {
+                    Console.Write(".");
+                    Image img = Image.FromFile(f);
+                    maxWidth = Math.Max(maxWidth, img.Width);
+                    maxHeight = Math.Max(maxHeight, img.Height);
+                    img.Dispose();
+                }
+                Console.WriteLine("\nLargest input image is {0} x {1}.", maxWidth, maxHeight);
+
+                if (fileCount < columns)
+                {
+                    Console.WriteLine("Single row, reducing column count to match file count.");
+                    columns = fileCount;
+                }
+
+                int width = columns * maxWidth;
+                int rows = (fileCount / columns) + ((fileCount % columns > 0) ? 1 : 0);
+                int height = rows * maxHeight;
+
+                Console.WriteLine("Output: {0} rows, {1} cols, {2} x {3} resolution.", rows, columns, width, height);
+
+                Bitmap sheet = new Bitmap(width, height);
+                using (Graphics gfx = Graphics.FromImage(sheet))
+                {
+                    int col = 0;
+                    int row = 0;
+                    foreach (string f in files)
+                    {
+                        Image img = Image.FromFile(f);
+                        Console.Write(".");
+
+                        // center it
+                        int x = (col * maxWidth) + (maxWidth / 2 - img.Width / 2);
+                        int y = (row * maxHeight) + (maxHeight / 2 - img.Height / 2);
+
+                        Rectangle srcRect = new Rectangle(0, 0, img.Width, img.Height);
+                        Rectangle destRect = new Rectangle(x, y, img.Width, img.Height);
+
+                        gfx.DrawImage(img, destRect, srcRect, GraphicsUnit.Pixel);
+
+                        img.Dispose();
+
+                        col++;
+                        if (col == columns)
+                        {
+                            col = 0;
+                            row++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("\nSaving:\n{0}", outputPath);
+                sheet.Save(outputPath);
+            }
+            else
+            {
+                Console.WriteLine("No PNG files found.");
+            }
+
+            Console.WriteLine("\nPRESS ANY KEY TO EXIT...");
+        }
+
+
+    }
+
+}
