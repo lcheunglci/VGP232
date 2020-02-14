4d74c17d17e1ab08ca3fbf4daff7c5d5d3433f56
Lawrence LCI <lcheung@lasallecollgevancouver.com>
2020-02-13 20:50:21 -0800

    [VGP232] Add a httpclient to retrieve data from the web api

---
+++ VGP232/Week6/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        const string API_URL = "https://api.chucknorris.io/";
+
+
+        private void GetJokeButtonPressed(object sender, RoutedEventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(API_URL);
+
+                // HTTP GET
+                var responseTask = httpClient.GetAsync("jokes/random?category=dev");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var res = result.Content.ReadAsStringAsync();
+                    res.Wait();
+
+                    string message = res.Result;
+                    if (!string.IsNullOrEmpty(message))
+                    {
+
+                        Joke joke = JsonConvert.DeserializeObject<Joke>(message);
+                        txtJoke.Text = joke.value;
+                    }
+                }
+
+            }
+        }
+    }
+}
