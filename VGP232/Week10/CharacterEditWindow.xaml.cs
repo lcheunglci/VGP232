28dd95eb64d643820793648bd53e9c878d4edac6
Lawrence Cheung <lcheunglci@gmail.com>
2020-09-10 20:12:40 -0700

    Add week 10 examples for data binding images, drag and drop and page navigation

---
+++ VGP232/Week10/CharacterEditWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Week10
+{
+    /// <summary>
+    /// Interaction logic for CharacterEditWindow.xaml
+    /// </summary>
+    public partial class CharacterEditWindow : Window
+    {
+        public Character MyCharacter { get; set; }
+
+        public CharacterEditWindow()
+        {
+            InitializeComponent();
+            MyCharacter = new Character();
+            this.DataContext = MyCharacter;
+        }
+
+        private void SelectPicture(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG Images| *.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MyCharacter.ImagePath = openFileDialog.FileName;
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
