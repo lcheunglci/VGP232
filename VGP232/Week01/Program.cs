996e8c57e4010ba7f7456e957d90f032535c8a71
Lawrence Cheung <lcheunglci@gmail.com>
2020-10-08 22:09:40 -0700

    Update Week01

--- VGP232/Week01/Program.cs
+++ VGP232/Week01/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace Week01
+namespace Week1
 {
+    // This is my project to show.
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Add a name.
+            string name = "Lawrence";
+
+            Console.WriteLine(name);
+
+            Console.WriteLine("What is your favorite number?");
+            string favNum = Console.ReadLine();
+            try
+            {
+                int favInt = int.Parse(favNum);
+                if (favInt == 9)
+                {
+                    Console.WriteLine("That's awesome!");
+                }
+                else
+                {
+                    Console.WriteLine("Cool.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("That is not a number. Good bye!");
+            }
         }
     }
 }
