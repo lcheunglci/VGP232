780435169246b1ea1173cf611f73c2589f0b7497
Lawrence Cheung <lcheunglci@gmail.com>
2020-11-12 20:36:04 -0800

    [VGP232] Add Week6 class encryption examples in aes

---
+++ VGP232/Week6/Crypto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Week6
+{
+    public enum CryptoAlgorithm { AES, RSA }
+
+    public class Crypto
+    {
+        AesCryptoServiceProvider aes;
+        // RSACryptoServiceProvider rsa;
+
+        CryptoAlgorithm mType;
+
+        public void Initialize(CryptoAlgorithm type)
+        {
+            mType = type;
+            aes = new AesCryptoServiceProvider();
+        }
+
+        public void Terminate()
+        {
+            if (aes != null)
+            {
+                aes.Dispose();
+            }
+        }
+
+
+        public byte[] Encrypt(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                return null;
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(data, 0, data.Length);
+                    cs.FlushFinalBlock();
+                }
+
+                return ms.ToArray();
+            }
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                return null;
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(data, 0, data.Length);
+                    cs.FlushFinalBlock();
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
